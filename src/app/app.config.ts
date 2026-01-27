ccaca1a0c42384b30438c3269d2b0af89b0dccb4
aleksandr.radaev <sasha555990@gmail.com>
2026-01-27 19:39:15 +0300

    инициализация проекта

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import {
+    ApplicationConfig,
+    provideZoneChangeDetection,
+    LOCALE_ID
+} from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { routes } from './app.routes'
+import { registerLocaleData } from '@angular/common'
+import localeRu from '@angular/common/locales/ru'
+
+registerLocaleData(localeRu)
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        { provide: LOCALE_ID, useValue: 'ru' }
+    ]
+}
