0a4930d8bbb04dd358a84aa6eda7cec172f808ea
aleksandr.radaev <sasha555990@gmail.com>
2026-01-29 18:46:12 +0300

    добавил превью разных состояний border-group в app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core'
+import { BorderGroupComponent } from "./shared/components/border-groupe/border-group.component";
+import { BorderGroupChildComponent } from "./shared/components/border-group-child/border-group-child.component";
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    imports: [BorderGroupComponent, BorderGroupChildComponent]
 })
 export class AppComponent {}
