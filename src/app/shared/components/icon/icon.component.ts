951cf3a47899c487e1a82bc1b06e01cd953c76b9
aleksandr.radaev <sasha555990@gmail.com>
2026-01-30 12:59:52 +0300

    добавил изменения в компонент Icon по комментариям PR

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -1,35 +1,24 @@
-import { Component, HostBinding, input } from '@angular/core'
+import {
+    ChangeDetectionStrategy,
+    Component,
+    HostBinding,
+    input
+} from '@angular/core'
 import { CommonModule } from '@angular/common'
-
-export type IconNames =
-    | 'flag'
-    | 'double-chevron-bottom-circle'
-    | 'double-chevron-top-circle'
-    | 'check'
-    | 'drag'
-    | 'calendar'
-    | 'file'
-    | 'link'
-    | 'plus'
-    | 'more'
-    | 'search'
-    | 'cross'
+import { IconNames } from './icon-names.type'
 
 @Component({
     selector: 'app-icon',
     standalone: true,
     imports: [CommonModule],
     templateUrl: './icon.component.html',
-    styleUrl: './icon.component.scss'
+    styleUrl: './icon.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IconComponent {
     name = input.required<IconNames>()
     color = input<string>()
 
-    @HostBinding('class')
-    get hostClass() {
-        return 'app-icon'
-    }
     @HostBinding('style.color')
     get hostStyleColor() {
         return this.color()
