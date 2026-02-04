4178dfc15f90479ea1be7ac3e17fa59514b716d0
aleksandr.radaev <sasha555990@gmail.com>
2026-02-04 14:24:53 +0300

    добавил ChangeDetectionStrategy.OnPush

--- src/app/shared/components/bordered-container/bordered-container.component.ts
+++ src/app/shared/components/bordered-container/bordered-container.component.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, Component, input } from '@angular/core'
+import { booleanAttribute, ChangeDetectionStrategy, Component, input } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common'
     standalone: true,
     imports: [CommonModule],
     templateUrl: './bordered-container.component.html',
-    styleUrl: './bordered-container.component.scss'
+    styleUrl: './bordered-container.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BorderedContainerComponent {
     public readonly withHoverMode = input(false, {
