d67122748a0ee3757d6418375ac9781c961bbf81
aleksandr.radaev <sasha555990@gmail.com>
2026-02-04 22:41:06 +0300

    корректировка компонента border-group и border-group-child
     для совместной работы с border-container

--- src/app/shared/components/border-group-child/border-group-child.component.ts
+++ src/app/shared/components/border-group-child/border-group-child.component.ts
@@ -10,13 +10,14 @@ import { CommonModule } from '@angular/common'
 import { IconComponent } from '../icon/icon.component'
 import { IconNames } from '../icon/icon-names.type'
 import { BorderGroupPosition } from './border-group-child-position.type'
+import { BorderedContainerComponent } from '../bordered-container/bordered-container.component'
 
 @Component({
     selector: 'app-border-group-child',
     standalone: true,
     templateUrl: 'border-group-child.component.html',
     styleUrl: 'border-group-child.component.scss',
-    imports: [CommonModule, IconComponent],
+    imports: [CommonModule, BorderedContainerComponent, IconComponent],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BorderGroupChildComponent {
@@ -33,9 +34,11 @@ export class BorderGroupChildComponent {
     public mouseLeave = output<void>()
 
     public onMouseEnter(): void {
+        if (this.disableHoverMode()) return
         this.mouseEnter.emit()
     }
     public onMouseLeave(): void {
+        if (this.disableHoverMode()) return
         this.mouseLeave.emit()
     }
 }
