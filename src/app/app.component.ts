6d50f0aea219c8cf636a35f99c9ec3eae4cd0e32
aleksandr.radaev <sasha555990@gmail.com>
2026-02-18 15:41:40 +0300

    корректировки отображения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,27 @@ import { Component, signal } from '@angular/core'
 import { BorderGroupComponent } from './shared/components/border-group/border-group.component'
 import { BorderGroupChildComponent } from './shared/components/border-group-child/border-group-child.component'
 import { BadgeComponent } from "./shared/components/badge/badge.component";
+import { BadgeComponent } from './shared/components/badge/badge.component'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [BorderGroupComponent, BorderGroupChildComponent, BadgeComponent]
+    imports: [
+        BorderGroupComponent,
+        BorderGroupChildComponent,
+        BadgeComponent,
+    ]
 })
 export class AppComponent {
 	protected bageState = signal(false)
+    protected bageState = signal(false)
 
 	protected toggle(): void {
 		this.bageState.set(!this.bageState())
 	}
+    protected toggle(): void {
+        this.bageState.set(!this.bageState())
+    }
 }
