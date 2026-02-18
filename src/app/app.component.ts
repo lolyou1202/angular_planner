b1033524dabdd33cf3cf859f2d46827b210fd65b
aleksandr.radaev <sasha555990@gmail.com>
2026-02-18 10:13:20 +0300

    добавил компонент badge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, signal } from '@angular/core'
 import { BorderGroupComponent } from './shared/components/border-group/border-group.component'
 import { BorderGroupChildComponent } from './shared/components/border-group-child/border-group-child.component'
+import { BadgeComponent } from "./shared/components/badge/badge.component";
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [BorderGroupComponent, BorderGroupChildComponent]
+    imports: [BorderGroupComponent, BorderGroupChildComponent, BadgeComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+	protected bageState = signal(false)
+
+	protected toggle(): void {
+		this.bageState.set(!this.bageState())
+	}
+}
