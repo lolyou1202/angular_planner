7d80121023132ef548fb80b19d91fee063673442
aleksandr.radaev <sasha555990@gmail.com>
2026-03-11 17:32:04 +0300

    корректировка task-group

--- src/app/features/tasks/components/task-group/task-group.component.ts
+++ src/app/features/tasks/components/task-group/task-group.component.ts
@@ -3,26 +3,19 @@ import { Group, Task } from '../../models/tasks.model'
 import { TaskCardComponent } from '../task-card/task-card.component'
 import { ButtonComponent } from '../../../../shared/components/button/button.component'
 import { DragDropService } from '../../../../shared/services/drag-drop.service'
-import { DropZoneDirective } from '../../../../shared/directives/drop-zone.directive'
 import { DropIndicatorDirective } from '../../../../shared/directives/drop-indicator.directive'
 
 @Component({
     selector: 'app-task-group',
     templateUrl: './task-group.component.html',
     styleUrl: './task-group.component.scss',
-    imports: [ButtonComponent, TaskCardComponent, DropZoneDirective, DropIndicatorDirective]
+    imports: [ButtonComponent, TaskCardComponent, DropIndicatorDirective]
 })
 export class TaskGroupComponent {
-    public readonly group = input.required<Group>()
-    public readonly groupId = computed(() => this.group().id)
-
     protected readonly dragDropService = inject(DragDropService<Task>)
 
-    protected readonly dropIndex = computed(() => {
-        const targetZoneId = this.dragDropService.targetZoneId()
-        if (targetZoneId !== this.groupId()) return -1
-        return this.dragDropService.targetIndex()
-    })
+    public readonly group = input.required<Group>()
+    public readonly groupId = computed(() => this.group().id)
 
     protected readonly showIndicator = computed(() =>
         this.dragDropService.isDragging()
